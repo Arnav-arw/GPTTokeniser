41f5e9e5d656484ceca79763d9bca88c4c50d6b9
Arnav Singhal <singhal.arnav@gmail.com>
2023-07-02 13:05:02 +0530

    Initial Commit

---
+++ GPTTokeniser/GPTTokeniserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTTokeniserApp.swift
+//  GPTTokeniser
+//
+//  Created by Arnav Singhal on 02/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GPTTokeniserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
